4837fb2a5beeba78fc82d77cf4c761bff6e194f5
jaunty wunder kind <jaunty@eldergods.com>
2021-10-11 02:58:51 -0400

    debianization of libneatvnc0

---
+++ debian/libneatvnc.doc-base.ex
@@ -0,0 +1,20 @@
+Document: libneatvnc
+Title: Debian libneatvnc Manual
+Author: <insert document author here>
+Abstract: This manual describes what libneatvnc is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/libneatvnc/libneatvnc.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/libneatvnc/libneatvnc.ps.gz
+
+Format: text
+Files: /usr/share/doc/libneatvnc/libneatvnc.text.gz
+
+Format: HTML
+Index: /usr/share/doc/libneatvnc/html/index.html
+Files: /usr/share/doc/libneatvnc/html/*.html

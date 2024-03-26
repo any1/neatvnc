47e714b2bf9e3322cc6895002391e664bcc6eacf
Andri Yngvason <andri@yngvason.is>
2024-03-26 10:38:03 +0000

    h264-v4l2m2m: Update copyright
    Raspberry Pi Ltd. have kindly allowed me to retain copyright for this work
    that they sponsored.

--- src/h264-encoder-v4l2m2m-impl.c
+++ src/h264-encoder-v4l2m2m-impl.c
@@ -1,5 +1,5 @@
 /*
- * Copyright ⓒ 2024 Raspberry Pi Ltd.
+ * Copyright (c) 2024 Andri Yngvason
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above

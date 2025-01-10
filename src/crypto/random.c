619f859296f5360e5bb36e0421a40237f51b2691
Norbert Schultz <norbert.schultz@reactivecore.de>
2025-01-10 17:40:04 +0100

    crypto: Use arc4random_buf if necessary
    Only linux supports getrandom, other platforms may support arc4random_buf.

--- src/crypto/random.c
+++ src/crypto/random.c
@@ -15,13 +15,24 @@
  */
 
 #include "crypto.h"
+#include "config.h"
 
 #include <stdint.h>
 
-// TODO: This is linux specific
+#if defined(HAVE_GETRANDOM)
 #include <sys/random.h>
+#elif defined(HAVE_ARC4RANDOM)
+#include <stdlib.h>
+#endif
+
 
 void crypto_random(uint8_t* dst, size_t len)
 {
+#if defined(HAVE_GETRANDOM)
 	getrandom(dst, len, 0);
+#elif defined(HAVE_ARC4RANDOM)
+	arc4random_buf(dst, len);
+#else
+#error No random generator available
+#endif
 }

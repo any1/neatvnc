80712f636b5f83049888aa3bee0d1248e6f2b9a4
Andrian Budantsov <a@hypersequent.com>
2026-03-04 15:23:49 +0400

    Move security result helpers from auth/common.c to server.c

--- src/auth/common.c
+++ src/auth/common.c
@@ -14,10 +14,8 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include "config.h"
 #include "auth/auth.h"
-#include "stream/stream.h"
-#include "rfb-proto.h"
-#include "common.h"
 #include "neatvnc.h"
 
 #include <string.h>
@@ -27,72 +25,6 @@
 #endif
 
 #define EXPORT __attribute__((visibility("default")))
-
-static int security_send_failure(struct nvnc_client* client,
-		uint32_t result_code, const char* reason_string)
-{
-	char buffer[256];
-
-	uint32_t* result = (uint32_t*)buffer;
-	*result = htonl(result_code);
-
-	size_t len;
-	if (reason_string) {
-		struct rfb_error_reason* reason =
-			(struct rfb_error_reason*)(buffer + sizeof(*result));
-		reason->length = htonl(strlen(reason_string));
-		strcpy(reason->message, reason_string);
-		len = sizeof(*result) + sizeof(*reason) + strlen(reason_string);
-	} else {
-		len = sizeof(*result);
-	}
-
-	stream_write(client->net_stream, buffer, len, close_after_write,
-			client->net_stream);
-
-	stream_ref(client->net_stream);
-
-	nvnc_client_close(client);
-	return 0;
-}
-
-int security_handshake_failed(struct nvnc_client* client, const char* username,
-		const char* reason_string)
-{
-	if (username)
-		nvnc_log(NVNC_LOG_INFO, "Security handshake failed for \"%s\": %s",
-				username, reason_string);
-	else
-		nvnc_log(NVNC_LOG_INFO, "Security handshake failed: %s",
-				reason_string);
-
-	const char* reason = client->rfb_minor_version >= 8 ? reason_string : NULL;
-	return security_send_failure(client, RFB_SECURITY_HANDSHAKE_FAILED,
-			reason);
-}
-
-int security_type_invalid(struct nvnc_client* client,
-		const char* reason_string)
-{
-	nvnc_log(NVNC_LOG_WARNING, "Connection rejected: %s", reason_string);
-
-	return security_send_failure(client, RFB_SECURITY_TYPE_INVALID,
-			reason_string);
-}
-
-int security_handshake_ok(struct nvnc_client* client, const char* username)
-{
-	if (username) {
-		nvnc_log(NVNC_LOG_INFO, "User \"%s\" authenticated", username);
-
-		strncpy(client->username, username, sizeof(client->username));
-		client->username[sizeof(client->username) - 1] = '\0';
-	}
-
-	uint32_t result = htonl(RFB_SECURITY_HANDSHAKE_OK);
-	return stream_write(client->net_stream, &result, sizeof(result), NULL,
-			NULL);
-}
 
 EXPORT
 bool nvnc_auth_creds_verify(const struct nvnc_auth_creds* creds,
--- src/server.c
+++ src/server.c
@@ -239,6 +239,72 @@ void close_after_write(void* userdata, enum stream_req_status status)
 	stream_destroy(stream);
 }
 
+static int security_send_failure(struct nvnc_client* client,
+		uint32_t result_code, const char* reason_string)
+{
+	char buffer[256];
+
+	uint32_t* result = (uint32_t*)buffer;
+	*result = htonl(result_code);
+
+	size_t len;
+	if (reason_string) {
+		struct rfb_error_reason* reason =
+			(struct rfb_error_reason*)(buffer + sizeof(*result));
+		reason->length = htonl(strlen(reason_string));
+		strcpy(reason->message, reason_string);
+		len = sizeof(*result) + sizeof(*reason) + strlen(reason_string);
+	} else {
+		len = sizeof(*result);
+	}
+
+	stream_write(client->net_stream, buffer, len, close_after_write,
+			client->net_stream);
+
+	stream_ref(client->net_stream);
+
+	nvnc_client_close(client);
+	return 0;
+}
+
+int security_handshake_failed(struct nvnc_client* client, const char* username,
+		const char* reason_string)
+{
+	if (username)
+		nvnc_log(NVNC_LOG_INFO, "Security handshake failed for \"%s\": %s",
+				username, reason_string);
+	else
+		nvnc_log(NVNC_LOG_INFO, "Security handshake failed: %s",
+				reason_string);
+
+	const char* reason = client->rfb_minor_version >= 8 ? reason_string : NULL;
+	return security_send_failure(client, RFB_SECURITY_HANDSHAKE_FAILED,
+			reason);
+}
+
+int security_type_invalid(struct nvnc_client* client,
+		const char* reason_string)
+{
+	nvnc_log(NVNC_LOG_WARNING, "Connection rejected: %s", reason_string);
+
+	return security_send_failure(client, RFB_SECURITY_TYPE_INVALID,
+			reason_string);
+}
+
+int security_handshake_ok(struct nvnc_client* client, const char* username)
+{
+	if (username) {
+		nvnc_log(NVNC_LOG_INFO, "User \"%s\" authenticated", username);
+
+		strncpy(client->username, username, sizeof(client->username));
+		client->username[sizeof(client->username) - 1] = '\0';
+	}
+
+	uint32_t result = htonl(RFB_SECURITY_HANDSHAKE_OK);
+	return stream_write(client->net_stream, &result, sizeof(result), NULL,
+			NULL);
+}
+
 static int handle_unsupported_version(struct nvnc_client* client)
 {
 	char buffer[256];
